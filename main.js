590672c8af69db4cb230bce130716939e8c475d2
Napoleon-Jahmiu <Napoleonjay09mail.com>
2020-03-26 22:58:24 +0100

    integrating payment gateway with paystack and paypal

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
-var = handler {
-    
+function payWithPaystack () {
+var  handler = PaystackPop.setup ({
+    key = 'pk_test_77b31b8f3ebf740c3220d053e6dc8a173215d933', 
+    email = 'Napoleonjay09@gmail.com',
+    amount = '200000',
+    metadata: {
+        custom_fields: [
+            {
+                displayName: "Mobile Number",
+                variableName: "mobileNumber",   
+                value: "+2349068884870",
+
+            }
+        ]
+    },
+
+    callback: function (response) {
+        // after the transfer haae been completed.
+        alert ('success. transaction ref is ' + response);
+    },
+
+    onClose: function () {
+        // When the User close the Payment Modal
+        alert ('Transaction Cancelled')
+    }
+});
+
+// This will open the paystack payment Modal
+handler.openIframe()
 }
