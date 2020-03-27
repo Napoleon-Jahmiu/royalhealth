22a35f5d1e5c312ddd920cd28974b363308d3876
Napoleon-Jahmiu <Napoleonjay09mail.com>
2020-03-27 11:55:43 +0100

    added the design for payment options

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
+// Sticky menu background
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 150) {
+      document.querySelector('#navbar').style.opacity = 0.9;
+    } else {
+      document.querySelector('#navbar').style.opacity = 1;
+    }
+  });
+  
+  
+  // Smooth Scrolling
+  $('#navbar a, .btn').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+  
+      const hash = this.hash;
+  
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top - 100
+        },
+        800
+      );
+    }
+  });
+
+
+
+
 function payWithPaystack () {
 var  handler = PaystackPop.setup ({
     key = 'pk_test_77b31b8f3ebf740c3220d053e6dc8a173215d933', 
