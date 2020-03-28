8916c2739dfb2533c6728b5307e8458c537b7d1f
Napoleon-Jahmiu <Napoleonjay09mail.com>
2020-03-28 18:05:38 +0100

    changed the buttons

--- main.js
+++ main.js
@@ -1,74 +1,59 @@
-// // Sticky menu background
-// window.addEventListener('scroll', function() {
-//     if (window.scrollY > 150) {
-//       document.querySelector('#navbar').style.opacity = 0.9;
-//     } else {
-//       document.querySelector('#navbar').style.opacity = 1;
-//     }
-//   });
-  
-  
-//   // Smooth Scrolling
-//   $('#navbar a, .btn').on('click', function(event) {
-//     if (this.hash !== '') {
-//       event.preventDefault();
-  
-//       const hash = this.hash;
-  
-//       $('html, body').animate(
-//         {
-//           scrollTop: $(hash).offset().top - 100
-//         },
-//         800
-//       );
-//     }
-//   });
-
-
-
-<script src="https://js.paystack.co/v1/inline.js"></script>
-<div id="paystackEmbedContainer"></div>
-
-<script>
-  PaystackPop.setup({
-   key: 'pk_test_77b31b8f3ebf740c3220d053e6dc8a173215d933',
-   email: 'customer@email.com',
-   amount: 10000,
-   container: 'paystackEmbedContainer',
-   callback: function(response){
-        alert('successfully subscribed. transaction ref is ' + response.reference);
-    },
+// Sticky menu background
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 150) {
+      document.querySelector('#navbar').style.opacity = 0.9;
+    } else {
+      document.querySelector('#navbar').style.opacity = 1;
+    }
   });
-</script
+  
+  
+  // Smooth Scrolling
+  $('#navbar ul li a, .btn').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+  
+      const hash = this.hash;
+  
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top - 100
+        },
+        800
+      );
+    }
+  });
 
 
-// function payWithPaystack (){
-// var  handler = PaystackPop.setup ({
-//     key = 'pk_test_77b31b8f3ebf740c3220d053e6dc8a173215d933', 
-//     email = 'Napoleonjay09@gmail.com',
-//     amount = '200000',
-//     metadata: {
-//         custom_fields: [
-//             {
-//                 displayName: "Mobile Number",
-//                 variableName: "mobileNumber",   
-//                 value: "+2349068884870",
 
-//             }
-//         ]
-//     },
 
-//     callback: function (response) {
-//         // after the transfer haae been completed.
-//         alert ('success. transaction ref is ' + response);
-//     },
+function payWithPaystack (){
+var  handler = PaystackPop.setup ({
+    key = 'pk_test_77b31b8f3ebf740c3220d053e6dc8a173215d933', 
+    email = 'Napoleonjay09@gmail.com',
+    amount = '200000',
+    metadata: {
+        custom_fields: [
+            {
+                displayName: "Mobile Number",
+                variableName: "mobileNumber",   
+                value: "+2349068884870",
 
-//     onClose: function () {
-//         // When the User close the Payment Modal
-//         alert ('Transaction Cancelled')
-//     }
-// });
+            }
+        ]
+    },
 
-// // This will open the paystack payment Modal
-// handler.openIframe()
-// }
+    callback: function (response) {
+        // after the transfer haae been completed.
+        alert ('success. transaction ref is ' + response);
+    },
+
+    onClose: function () {
+        // When the User close the Payment Modal
+        alert ('Transaction Cancelled')
+    }
+});
+
+// This will open the paystack payment Modal
+handler.openIframe()
+}
